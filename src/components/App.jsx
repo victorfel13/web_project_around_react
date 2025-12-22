e61667f7ef1dfa7a4ea83dbd87c0280ce14c01d0
Victor <vokocer@gmail.com>
2025-12-22 01:13:20 -0600

    appjs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
-import CurrentUserContext from "./contexts/CurrentUserContext";
-import api from "./utils/api";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./Footer/Footer";
+import CurrentUserContext from "../contexts/CurrentUserContext";
+import api from "../utils/api";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
